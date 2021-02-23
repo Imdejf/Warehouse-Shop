c0e14e252b4b78f1735d9195ba6ea97ad22ce803
Dawid <jablonskidawid0202@gmail.com>
2021-02-23 21:14:49 +0100

    Add all files jquery and add index.cshtml

--- src/WarehauseShop.WebUI/Controllers/HomeController.cs
+++ src/WarehauseShop.WebUI/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using WarehauseShop.WebUI.Models;
 
 namespace WarehauseShop.WebUI.Controllers
 {
@@ -28,10 +23,6 @@ namespace WarehauseShop.WebUI.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
--- src/WarehauseShop.WebUI/Startup.cs
+++ src/WarehauseShop.WebUI/Startup.cs
@@ -24,6 +24,7 @@ namespace WarehauseShop.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
