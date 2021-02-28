45b2b179e8a75aa86e308737831e246e4f252d58
Dawid <jablonskidawid0202@gmail.com>
2021-02-28 20:19:25 +0100

    add SD class

---
+++ src/WarehauseShop.Domain/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehauseShop.Domain
+{
+    public class SD
+    {
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+    }
+}
