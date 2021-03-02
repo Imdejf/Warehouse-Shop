68290a7d58351d1e1eae623c71e4fcc43797470e
Dawid <jablonskidawid0202@gmail.com>
2021-03-02 22:46:04 +0100

    Add UserNotFoundException

---
+++ src/WarehauseShop.Application/Common/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WarehauseShop.Application.Common.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public string Username { get; set; }
+        public UserNotFoundException(string username)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string message, string username) : base(message)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string message, string username, Exception innerException) : base(message, innerException)
+        {
+            Username = username;
+        }
+    }
+}
