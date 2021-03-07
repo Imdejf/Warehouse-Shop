82c87e92d1c04c892698f2ab295cecfc69718a60
Dawid <jablonskidawid0202@gmail.com>
2021-03-07 17:50:42 +0100

    Create category Index

---
+++ src/WarehauseShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehauseShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/");
+            }
+        }
+    }
+}
---
+++ src/WarehauseShop.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace WarehauseShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/");
+            }
+        }
+    }
+}
