4c4ff9304223868f6e7550e01b1fe3ebed7fe566
Dawid <jablonskidawid0202@gmail.com>
2021-03-08 18:51:44 +0100

    Add ICategoryRepository

---
+++ src/WarehauseShop.Domain/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarehauseShop.Domain.Entites;
+
+namespace WarehauseShop.Domain.IRepository
+{
+    public interface ICategoryRepository : IRepositoryAsync<Category>
+    {
+        void Update(Category category);
+    }
+}
