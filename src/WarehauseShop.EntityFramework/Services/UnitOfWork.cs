3e029125e75d3043e1fc51956fcb89fac434a615
Dawid <jablonskidawid0202@gmail.com>
2021-03-08 19:13:48 +0100

    Add UnitOfWork

--- src/WarehauseShop.EntityFramework/Services/UnitOfWork.cs
+++ src/WarehauseShop.EntityFramework/Services/UnitOfWork.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WarehauseShop.Domain.IRepository;
 
 namespace WarehauseShop.EntityFramework.Services
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly WarehauseDbContext _dbContext;
+        public UnitOfWork(WarehauseDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
